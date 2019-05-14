e34be2bcf5ffa51d29d9e15c1243b6284ad5ed0d
xiao0811 <panxiao0811@gmail.com>
2019-05-14 23:16:22 +0800

    gin/iris

--- src/http/gin/main.go
+++ src/http/gin/main.go
@@ -1,15 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "github.com/kataras/iris"
 
 func main() {
-	app := gin.New()
+	//app := gin.New()
+	//app.Use(gin.Logger())
+	//app.GET("/xiaosha", func(context *gin.Context) {
+	//	context.JSON(200, gin.H{
+	//		"name": "xiaosha",
+	//		"age": 20,
+	//	})
+	//})
+	//app.Run()
 
-	app.GET("/xiaosha", func(context *gin.Context) {
-		context.JSON(200, gin.H{
+	app := iris.New()
+	app.Logger()
+
+	app.Get("/xiaosha", func(context iris.Context) {
+		_, _ = context.JSON(iris.Map{
 			"name": "xiaosha",
 			"age": 20,
 		})
 	})
-	app.Run()
+
+	app.Run(iris.Addr(":8080"))
 }
